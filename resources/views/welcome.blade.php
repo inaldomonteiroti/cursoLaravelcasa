261cef52394df409fdb90a97dbfdc0e67e6dc172
Inaldo Monteiro <inaldomonteiroti@gmail.com>
2020-10-11 13:28:33 -0300

    Ajuste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    CursoLaravel2
+                    CursoLaravel
                 </div>
 
                 <div class="links">
